d8c43507a43236ddb8c53d37f8654408391335c7
daria-shab <daria.shabatska@ucu.edu.ua>
2021-11-17 18:23:39 +0200

    added ids and connection

--- src/main/java/FlowerStore/Receiver.java
+++ src/main/java/FlowerStore/Receiver.java
@@ -1,6 +1,10 @@
 package FlowerStore;
 
 public class Receiver implements User{
+    int id;
     public void update(String status){
+    }
+    public Receiver(){
+        this.id = (int)Math.floor(Math.random()*(1000*1000*1000-1000*1000)+1000*1000);
     }
 }
--- src/main/java/FlowerStore/Sender.java
+++ src/main/java/FlowerStore/Sender.java
@@ -1,6 +1,10 @@
 package FlowerStore;
 
 public class Sender implements User{
+    int id;
     public void update(String status){
+    }
+    public Sender(){
+        this.id = (int)Math.floor(Math.random()*(1000*1000*1000-1000*1000)+1000*1000);
     }
 }
